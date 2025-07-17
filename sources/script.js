5ded83fac1a32a73951926b4aff15c7aaaaa2937
digitradiart <digitradiart@gmail.com>
2025-07-17 17:07:36 +0700

    fix swiper js

--- sources/script.js
+++ sources/script.js
@@ -1,8 +1,23 @@
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 4,
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
+  slidesPerView: 2, // default untuk mobile
+  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    700: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
   },
 });
